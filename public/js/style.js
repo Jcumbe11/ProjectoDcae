65e67cdbea2adb908198313f47088438dcfe69e8
Jcumbe11 <135236214+Jcumbe11@users.noreply.github.com>
2023-11-15 20:16:59 -0800

    Update style.js

--- public/js/style.js
+++ public/js/style.js
@@ -1,22 +1,39 @@
 const doc = [
     {
         doc1:"reg.pdf",
-        doc2:""
+        doc2:"calendario.pdf",
+        doc3: "taxas.pdf"
 
     }
 ];
 
 
 var viewpdf = $("viewpdf");
-
+// var viewpdf1 = $("viewpdf1");
+const imagelinkcalendario = document.getElementById("imagelinkcalendario");
 
 const imagelink =  document.getElementById("imagelink");
+const taxas = document.getElementById("taxas");
 
-if(imagelink!=null){
+if(imagelink!==null){
     imagelink.addEventListener('click',function(){
         PDFObject.embed(doc[0].doc1,viewpdf);
     });
 }
+
+if(imagelinkcalendario!==null){
+    imagelinkcalendario.addEventListener('click',function(){
+        PDFObject.embed(doc[0].doc2,viewpdf);
+    });
+}
+
+if(taxas!==null){
+    taxas.addEventListener('click',function(){
+        PDFObject.embed(doc[0].doc3,viewpdf);
+    });
+}
+
+
 
 
 
